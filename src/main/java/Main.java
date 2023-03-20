4ff8822a80591246f3ae55111b0707b4d4236c7e
krein15 <kreinwork1@gmail.com>
2023-03-20 21:04:49 +0500

    Проект с правками

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
         int numberOfFriends = -1;
-        double resultSum = 0;
         String addedProduct ="";
 
 
@@ -19,6 +18,7 @@ public class Main {
             System.out.println("На сколько человек делить счет?");
             if (scanner.hasNextInt()) {
                 numberOfFriends = scanner.nextInt();
+                scanner.nextLine();
                 if (numberOfFriends == 1) {
                     System.out.println("Количество равно 1, нет смысла делить счет.");
                 } else if (numberOfFriends < 1) {
@@ -33,8 +33,9 @@ public class Main {
         }
 
             while(true){
+
                 System.out.println("Введите название товара. Для того, чтобы получить итог напишите 'Завершить'");
-                addedProduct = scanner.next();
+                addedProduct = scanner.nextLine();
                     if (addedProduct.equalsIgnoreCase("Завершить")){
                         break;}
 
@@ -45,11 +46,16 @@ public class Main {
 
                             if(scanner.hasNextDouble()){
                                 double priceOfProduct = scanner.nextDouble();
-                                calculator.counting(addedProduct, priceOfProduct);
-                                resultSum = resultSum + priceOfProduct;
-                                System.out.println("Товар добавлен!");
-                                System.out.println("Вы можете добавить еще товар, для этого введите любой симовл или напишите 'Завершить'");
                                 scanner.nextLine();
+                                if (priceOfProduct<0){
+                                    System.out.println("Некорректное значение повторите ввод.");
+                                }
+                                else {
+                                    calculator.counting(addedProduct, priceOfProduct);
+                                    System.out.println("Товар добавлен!");
+                                    System.out.println("Вы можете добавить еще товар, для этого введите любой симовл или напишите 'Завершить'");
+
+                                }
 
 
 
@@ -61,21 +67,34 @@ public class Main {
 
         }
 
-                String rubForm ="";
-                if (calculator.sum < 2){
+                String rubForm ="рублей";
+                String resultRubForm ="рублей";
+                int intSum = (int)calculator.sum;
+                int resultSum =(int)calculator.sum/numberOfFriends;
+                if (intSum % 10 ==1){
                     rubForm = "рубль";
                 }
-                else if (calculator.sum < 5){
+                else if ((intSum %10 >= 2) && (intSum %10 <=4)){
                     rubForm = "рубля";
                 }
-                else {
+                else if ((intSum % 100 >=11)&& (intSum %100 <=20)){
                     rubForm = "рублей";
+                    if (resultSum % 10 ==1){
+                        resultRubForm = "рубль";
+                    }
+                    else if ((resultSum %10 >= 2) && (resultSum %10 <=4)){
+                        resultRubForm= "рубля";
+                    }
+                    else if ((resultSum % 100 >=11)&& (resultSum %100 <=20)){
+                        resultRubForm = "рублей";}
 
                 }
                 String result = String.format("%.2f" , calculator.sum/numberOfFriends);
+
                 System.out.println(calculator.addedProducts);
-                System.out.println("Итоговая стоимость заказа : " + resultSum);
-                System.out.println("Каждый человек должен заплатить по " +result + " "+ rubForm);
+                System.out.println("Итоговая стоимость заказа : " + calculator.sum +" " +rubForm);
+
+                System.out.println("Каждый человек должен заплатить по " +result + " "+ resultRubForm);
 
 
     }
